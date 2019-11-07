2e38110ba9c4fd7d6e04099f48973ae17918cf6b
theboss <diogo.h.p.almeida@gmail.com>
2019-11-07 17:42:55 +0000

    AOCL

--- src/dataStructures/AOCL.java
+++ src/dataStructures/AOCL.java
@@ -5,19 +5,56 @@ public class AOCL {
 	DoublyLinkedList<Integer>[] elements;
 	int size;
 	
+	@SuppressWarnings("unchecked")
+	public AOCL(int size) {
+		elements = new DoublyLinkedList[size];
+		this.size = size;
+		initialize(size);
+	}
 	
 	public void initialize(int size){
 		this.size = size;
+		
 		for(int i = 0 ; i < size ; i++) {
 			elements[i] = new DoublyLinkedList<Integer>();
 		}
 	}
-	
 	public void insert(int index,int element) {
-		elements[index].addFirst(element);
+		elements[index].addLast(element);
 	}
 	
-	public  DoublyLinkedList<Integer> concatAll() {
-		return null;
+	public DListNode<Integer> concatAll() {
+		DListNode<Integer> last= null,next = null, head = null;
+		int lastIndex = -1,nextIndex = 0;
+		boolean done = false;
+		
+		while(!done) {
+			if(last == null) {
+				lastIndex++;
+				last = elements[lastIndex].tail;
+			}
+			if(next == null && last != null) {
+				nextIndex++;
+				next = elements[nextIndex].head;					
+			}else if(next == null && last == null){
+				nextIndex++;
+			}
+			if(last != null && next != null) {
+				if(head == null) {
+					head = elements[lastIndex].head;
+				}
+				last.next = next;
+				next.previous = last;
+				last = elements[nextIndex].tail;
+				next = null;
+			}
+			if(nextIndex >= size-1) {
+				done = true;
+			}
+		}
+		
+		initialize(size);
+		
+		return head;
 	}
 }
--- src/dataStructures/Main.java
+++ src/dataStructures/Main.java
@@ -1,11 +1,30 @@
 package dataStructures;
 
 public class Main {
- public static void main(String args[]) {
-	 
-	 @SuppressWarnings("unchecked")
-	Queue<String>[] buckets = new Queue[10];
-	 
-	
- }
+	public static void main(String args[]) {
+		int size = 10;
+
+		AOCL test = new AOCL(size);
+		
+		
+		test.insert(1, 1);
+		test.insert(1, 2);
+		test.insert(1, 3);
+		test.insert(1, 4);
+		test.insert(1, 5);
+		test.insert(7, 6);
+		test.insert(8, 7);
+		test.insert(8, 8);
+
+		DListNode<Integer> node = test.concatAll();
+		
+		if(node.next == null) {
+			System.out.println("something is wrong");
+		}
+		
+		while (node != null) {
+			System.out.println(node.getElement().toString()+" ;");
+			node = node.next;
+		}
+	}
 }
